b3bbb4aca812f2374dcc7d1e4880881632b0162c
Elijah Diller <ediller@nd.edu>
2025-10-25 02:16:09 -0400

    Fix solution with problem 22 solution, ran into issue with counting how many names were in list.

--- problem_22.cpp
+++ problem_22.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main(){
     long long total = 0;
-    string names[5000];
+    string names[5163];
     ifstream input("names.txt");
     ofstream output("names_rev.txt");
     char char_rem = '"';
@@ -28,16 +28,16 @@ int main(){
     ifstream file("names.txt");
     int word_count = 0;
     string line, word;
-    while(getline(file, line) and word_count < 5000){
+    while(getline(file, line) and word_count < 5163){
         stringstream ss(line);
-        while(getline(ss, word, ',') and word_count < 5000){
+        while(getline(ss, word, ',') and word_count < 5163){
             word.erase(0, word.find_first_not_of(" \t"));
             word.erase(word.find_last_not_of(" \t") + 1);
             names[word_count++] = word;
         }
     }
-    sort(names, names + 5000);
-    for(int i = 1; i < 5001; i++){
+    sort(names, names + 5163);
+    for(int i = 1; i < 5164; i++){
         int score = 0;
         for(int j = 0; j < names[i-1].length(); j++){
             score += ((int)names[i-1][j])-64;
